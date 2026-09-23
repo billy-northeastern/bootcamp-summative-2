76c814c4b4de93b97174d4b3cee17214ab6902a1
Sameerah Mahmood <sameerah.mahmood@fdmgroup.com>
2026-09-23 13:08:10 +0100

    application file responsible for running and configuring the API

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from database import engine, SessionLocal
 from models import Base, Vehicle
 from data_loader import load_vehicles_from_csv
 
 app = Flask(__name__)
+#creates and verifies JWT tokens
+app.config["JWT_SECRET_KEY"] = "63090eb8c8186b068f08ca858f72ecceff56b138384d43ca58864c589b4c108e"
+jwt = JWTManager(app)
 
+@jwt.unauthorized_loader
+def invalid_token_callback(error):
+    return {
+        "Error": "Authentication token is required. Please try again."
+    }, 401
 Base.metadata.create_all(bind=engine)
 
 @app.teardown_appcontext
 def remove_session(exception=None):
     SessionLocal.remove()
 
-from routes.vehicles import *
-from routes.search import *
-
+from routes.authorisation import * 
+import routes.vehicles
+import routes.search
 
 
 if __name__ == "__main__":
