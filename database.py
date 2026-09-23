42fd3ec2646ce4763a054cbf19b7a3f8eb1253e4
Sameerah Mahmood <sameerah.mahmood@fdmgroup.com>
2026-09-23 12:37:27 +0100

    handles database connectivity and session handling

--- database.py
+++ database.py
@@ -8,4 +8,5 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 # establishes database sessions, ensuring that each request uses its own connection to the database 
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
+print("test")
 
